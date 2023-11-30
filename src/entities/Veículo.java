dfbf7b4946705a8aadcbdbc3a84bcd93e2763412
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-29 22:42:00 -0300

    Criada classe veículo

---
+++ src/entities/Veículo.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Veículo {
+	
+	private String modelo;
+	
+	public Veículo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+}
